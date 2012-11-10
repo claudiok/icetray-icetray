3b923f2e45e36d45f4cbd4cbc4ae0619f265e20a
Jakob van Santen <jvansanten@gmail.com>
2012-11-10 02:55:28 +0000

    I3Logger can be subclassed in Python.
    git-svn-id: http://code.icecube.wisc.edu/icetray/projects/icetray/branches/logging@1456 579d543e-85e3-4578-a5e8-1e6a993b7602

--- private/pybindings/I3Logging.cxx
+++ private/pybindings/I3Logging.cxx
@@ -11,12 +11,49 @@
 #include <icetray/I3Logging.h>
 
 using namespace boost::python;
+ 
+struct I3LoggerWrapper : public I3Logger, public wrapper<I3Logger> {
+	virtual void Log(I3LogLevel level, const char *unit, const char *file,
+	    int line, const char *func, const char *format, ...)
+	{
+		va_list args;
+		
+		va_start(args, format);
+		int messagesize = vsnprintf(NULL, 0, format, args);
+		
+		char log_message[messagesize + 1];
+		log_message[0] = 0;
+		
+		va_start(args, format);
+		vsprintf(&log_message[strlen(log_message)], format, args);
+		
+		if (override f = this->get_override("log")) {
+			f(level, unit, file, line, func, std::string(log_message));
+		} else {
+			PyErr_SetString(PyExc_NotImplementedError, "I3LoggerBase subclasses must implement log()");
+			throw error_already_set();
+		}
+	}
+};
 
 void register_I3Logging()
 {
 	class_<I3Logger, boost::shared_ptr<I3Logger>, boost::noncopyable>
 	    ("I3Logger", "Logging base class", no_init)
 		.add_static_property("global_logger", &GetIcetrayLogger, &SetIcetrayLogger)
+	;
+	
+	enum_<I3LogLevel>("I3LogLevel")
+		.value("LOG_TRACE", LOG_TRACE)
+		.value("LOG_DEBUG", LOG_DEBUG)
+		.value("LOG_INFO",  LOG_INFO)
+		.value("LOG_WARN",  LOG_WARN)
+		.value("LOG_ERROR", LOG_ERROR)
+		.value("LOG_FATAL", LOG_FATAL)
+	;
+	
+	class_<I3LoggerWrapper, boost::shared_ptr<I3LoggerWrapper>, boost::noncopyable>
+	    ("I3LoggerBase", "Base class for Python-side logging implementations")
 	;
 
 }
