36533b0a45e180444343126388dc650b07273cc6
Alex Olivas <olivas@icecube.umd.edu>
2015-02-12 16:38:52 +0000

    Latest OMKey and div by zero bugfix in I3Tray::PrintUsage, for old icerec releases.
    git-svn-id: http://code.icecube.wisc.edu/icetray/projects/icetray/releases/V13-12-01@2067 579d543e-85e3-4578-a5e8-1e6a993b7602

--- private/pybindings/I3Tray.py
+++ private/pybindings/I3Tray.py
@@ -251,7 +251,7 @@ class I3Tray(icetray.I3Tray):
             print("%40s: %6u calls to physics %9.2fs user %9.2fs system" % (k, pusage.ncall, pusage.usertime, pusage.systime))
 
             acc_time += pusage.systime + pusage.usertime
-            if acc_time/total_time > fraction:
+            if total_time > 0 and acc_time/total_time > fraction:
                 break
         print('-'*99)
 
